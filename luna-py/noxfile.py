beea76aa81afa3aa9dd88374a31409b55decb662
Fiona Klute <fiona.klute@gmx.de>
2025-04-11 17:33:02 +0200

    luna-py: Add pyproject.toml so "python3 -m build" works

--- luna-py/noxfile.py
+++ luna-py/noxfile.py
@@ -4,6 +4,5 @@ import nox
 @nox.session(python=['3.13'])
 def ci(session):
     """Build and run tests."""
-    session.install('maturin', 'pytest')
-    session.run('maturin', 'develop')
+    session.install('.', 'pytest')
     session.run('pytest', '-v')
